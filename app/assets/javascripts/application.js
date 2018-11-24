30dd4886a7c77607c11f1ab19bc86d76db021c59
rada0624 <tetravoxx@gmail.com>
2018-11-24 21:40:08 +0900

    update:マイページのカレンダーから投稿内容をモーダル表示できるように修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,15 +13,16 @@
 //= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
 
 // 大カテゴリと小カテゴリを連動する処理
 $(function($) {
-  // 小カテゴリの要素を変数に入れる
-  var $children = $('#children');
-  // 後のイベントで、不要なoption要素を削除するため、オリジナルを取得しておく
-  var original = $children.html();
+    // 小カテゴリの要素を変数に入れる
+    $children = $('#children');
+    // 後のイベントで、不要なoption要素を削除するため、オリジナルを取得しておく
+    original = $children.html();
+
+    $children.attr('disabled', 'disabled');
 
   $('#parent').change(function() {
     //選択された大カテゴリのvalueを取得し変数に入れる
@@ -34,8 +35,34 @@ $(function($) {
 
       //valueと異なるdata-valを持つ要素を削除
       if (val1 != val2) {
-        $(this).remove();
+         $(this).not(':first-child').remove();
       }
     });
+    //地方側のselect要素が未選択の場合、都道府県をdisabledにする
+    if ($(this).val() == "") {
+      $children.attr('disabled', 'disabled');
+    } else {
+      $children.removeAttr('disabled');
+    }
+  });
+});
+
+// モーダルウィンドウ
+$(function(){
+  // モーダルウィンドウが開くときの処理
+  $(".modalOpen").click(function(){
+
+    var navClass = $(this).attr("class"),
+        href = $(this).attr("href");
+
+        $(href).fadeIn();
+    $(this).addClass("open");
+    return false;
+  });
+  // モーダルウィンドウが閉じるときの処理
+  $(".modalClose").click(function(){
+    $(this).parents(".modal").fadeOut();
+    $(".modalOpen").removeClass("open");
+    return false;
   });
 });
